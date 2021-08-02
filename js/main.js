1185e9b4a567d290cce47b6134d1d8a72b5c0b93
Your Namemauler2007 <mauler2007@gmail.com>
2021-08-02 08:37:52 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,34 @@
+// task1
+var money;
+money = 1000;
+var income;
+income = 'deposit';
+var addExpenses;
+addExpenses = 'taxi , courses , restourant';
+var deposit;
+deposit = true;
+var mission;
+mission = 1000000;
+var period;
+period = 12;
+
+var budgetMounth = 1000;
+var budgetDay = budgetMounth / 30;
+
+// task2-1
+console.log(typeof (money));
+console.log(typeof (income));
+console.log(typeof (deposit));
+
+// task2-2
+console.log(addExpenses.length);
+
+// task2-3
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + ' гривен');
+
+// task2-4
+console.log(addExpenses.toLowerCase().split(','));
+
+// task2-5
+console.log(budgetDay);
