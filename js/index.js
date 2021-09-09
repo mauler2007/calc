baf9ddbdaaca6500fa366d2ccf30241948d3692d
Your Namemauler2007 <mauler2007@gmail.com>
2021-09-09 09:34:03 +0300

    add hw9

---
+++ js/index.js
@@ -0,0 +1,67 @@
+'use strict'
+// // a)
+// const start = document.getElementById('start');
+// // console.log(start);
+
+// // b)
+// const sumExtraInome = document.getElementsByTagName('income-items button');
+// console.log(sumExtraInome);
+
+// const sumNecessaryExpenses = document.getElementsByTagName('expenses-items button');
+// console.log(sumNecessaryExpenses);
+
+// // c)
+// const depositCheck = document.getElementById('deposit-check');
+// console.log(depositCheck);
+
+
+// const getExtraIncome = document.querySelectorAll('.additional_income-item');
+// console.log(getExtraIncome);
+
+const budgetDayValue = document.querySelectorAll('.budget_day-value');
+console.log(budgetDayValue);
+
+const expensesMonthValue = document.getElementsByClassName('.expenses_month-value');
+console.log(expensesMonthValue);
+
+const additionalIncomeValue = document.getElementsByClassName('.additional_income-value');
+console.log(additionalIncomeValue);
+
+const incomePeriodValue = document.getElementsByClassName('.income_period-value');
+console.log(incomePeriodValue);
+
+const targetMonthValue = document.getElementsByClassName('.target_month-value');
+console.log(targetMonthValue);
+
+
+
+const budgetMonthValue = document.querySelector('.budget_month-value');
+console.log(budgetDayValue);
+
+const cancel = document.querySelector('#cancel');
+console.log(cancel);
+
+const rangePeriodSelect = document.querySelector('.period-select');
+console.log(rangePeriodSelect);
+
+const extraIncome = document.querySelector('.income-items .income-title');
+console.log(extraIncome);
+
+const sumIncomeAmount = document.querySelector('.income-items .income-amount');
+console.log(sumIncomeAmount);
+
+
+const sumExpensesAmount = document.querySelector('.expenses-items .expenses-amount');
+console.log(sumIncomeAmount);
+
+
+
+// const extraIncome = document.querySelector('.income-items .income-title');
+// console.log(extraIncome);
+
+
+
+
+
+
+
